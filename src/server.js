918e8816af7118ad377029b0d051d63f8c02a76a
N J <navesh.professional@gmail.com>
2025-05-04 19:47:41 +0530

    Dependency changes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
-const express = require('express')
+import express from 'express'
+import appRoutes from './routes.js' 
 
 const app = express()
 const PORT = 8080;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World")
-});
+const STATUS={
+    'SUCCESS':'OK',
+    'FAILURE':'ERROR'
+}
+
+app.use(express.json())
+
+app.use('/v1',appRoutes)
+
 
 app.listen(PORT,()=>{
     console.log("Server Listening on port :",PORT)  
