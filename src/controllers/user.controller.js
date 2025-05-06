484ad979478d30fa367f697ed16bf339b236df02
Navesh Jaiswal <navesh.professional@gmail.com>
2025-05-06 19:02:27 +0530

    pino logger

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import { StatusCodes } from "http-status-codes";
+import pino from 'pino';
+
+const logger = pino();
 
 import userService from '../services/user.service.js'
 
@@ -9,8 +12,9 @@ const STATUS={
 
 const getAllUsers=(req,res)=>{
     const users = userService.getAllUsers()
-
+    
     if(users.length){
+        logger.info('Retrieving all Users...')
         return res.status(StatusCodes.OK).send(users)
     }
     return res.status(StatusCodes.NOT_FOUND).send(
@@ -24,8 +28,8 @@ const getAllUsers=(req,res)=>{
 const getUser=(req,res)=>{
     const id=parseInt(req.params.id,10);
     const user = userService.getUser(id)
-
     if(user){
+        logger.info('Retrieving User...')
         return res.status(StatusCodes.OK).send(
             {
                 status:STATUS.success,
@@ -45,6 +49,7 @@ const getUser=(req,res)=>{
 const addUser=(req,res)=>{
     const {body:user} = req;
 
+    logger.info('Creating a User...')
     const addedUser = userService.addUser(user);
     
     return res.status(StatusCodes.CREATED).send({
@@ -59,7 +64,9 @@ const updateUser = (req,res)=>{
     const id = parseInt(req.params.id,10 );
     const updatedUser = userService.updateUser(id,user);
     
+    
     if(updatedUser){
+        logger.info('Updating the User...')
         return res.status(StatusCodes.OK).send({
             status:STATUS.success,
             message:updatedUser,
@@ -77,6 +84,7 @@ const deleteUser=(req,res)=>{
     const id=parseInt(req.params.id,10)
     const user = userService.getUser(id);
     if(user){
+        logger.info('Deleting a User...')
         userService.removeUser(id);
         return res.status(StatusCodes.OK).send({
             status:STATUS.success,
